b2e6f41f7bfde6cf2e294079f88a2366a0d7bfe6
hiideehoo <hiideeho@gmail.com>
2026-02-25 17:17:38 -0600

    proto-submission

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
 import './App.css';
+import {useState} from 'react';
+import movies from './mock-data/movies';
+
+
+
+const genreList = document.getElementById("genreList");
+genreList.addEventListener("click", function(value) {
+    movies.filter((movie) => (movie.genre === value));
+	return <p>{movies}</p>
+})
 
 function App() {
-	return <></>;
+
+	const [selectedGenre, setSelectedGenre] = useState("");
+
+const filteredMovies = movies.filter(
+	(movie) => movie.genre === selectedGenre
+);
+
+	return <>
+	<br>
+        <select value={selectedGenre} onChange={(e) => setSelectedGenre(e.target.value)}>
+            <option value="Action">Action</option>
+            <option value="Adventure">Adventure</option>
+            <option value="Comedy">Comedy</option>
+            <option value="Drama">Drama</option>
+            <option value="Fantasy">Fantasy</option>
+			<option value="Horror">Horror</option>
+            <option value="Mystery">Mystery</option>
+            <option value="Romance">Romance</option>
+            <option value="Science Fiction">Science Fiction</option>
+            <option value="Thriller">Thriller</option>
+        </select>
+
+		{filteredMovies.map((movie) => (
+			<p key={movie.title}>{movie.title}</p>
+		))}
+    </br>
+	</>;
 }
 
 export default App;
